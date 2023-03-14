16f99710a860e5c767342275a6cf587d5ad7aad1
Oliver Brown <oliverbrodev@gmail.com>
2023-03-14 07:35:04 -0400

    feat: :sparkles: finish socket io implementation

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,22 +1,84 @@
+import { JwtService } from '@nestjs/jwt';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
+import { NewMessageDto } from './dtos/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  //INFO: socket decorator for socket io.
+  @WebSocketServer() wss: Server;
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  handleConnection(client: Socket) {
-    console.log('client connected', client.id);
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    //INFO: socket join user to a room name by string, ids, email etc.
+    //client.join('room');
+    //client.join(client.id);
+    //client.join(user.email);
+
+    //INFO: socket emit to all users inside a room
+    //this.wss.to('room').emit('')
+
+    //INFO: socket io emit values.
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 
   handleDisconnect(client: Socket) {
-    console.log('client disconnected', client.id);
+    this.messagesWsService.removeClient(client.id);
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  //INFO: nest socket io decorator for listening clients emits
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+    //INFO: socket emit for a specific client
+    //client.emit('message-from-server', {
+    //  fullName: '',
+    //  message: payload.message || 'no-message!!',
+    //});
+
+    //INFO: socket emit to all clients but the sender
+    //client.broadcast.emit('message-from-server', {
+    //  fullName: '',
+    //  message: payload.message || 'no-message!!',
+    //});
+
+    //INFO: socket emit to all client in a rom
+    //this.wss.to('');
+
+    //INFO: socket emit to all
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message!!',
+    });
   }
 }
